27de4ae17f549094b32393940c6ed00fd6be9b7c
User <User@DESKTOP-BOSJB4J>
2024-03-26 06:29:07 +0900

    QQ

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -1,9 +1,68 @@
 package com.javaex.ex10;
 
 public class Book {
+	private int bookNo;
+	private String title;
+	private String author;
+	private int stateCode;
+
+	public Book(int bookNo, String title, String author) {
+		this.bookNo = bookNo;
+		this.title = title;
+	    this.author = author;
+	    this.stateCode = 1; // Initializing stateCode to 1 (available)
+	    }
+
+	    // Getters and Setters
+	    public int getBookNo() {
+	        return bookNo;
+	    }
+
+	    public void setBookNo(int bookNo) {
+	        this.bookNo = bookNo;
+	    }
+
+	    public String getTitle() {
+	        return title;
+	    }
+
+	    public void setTitle(String title) {
+	        this.title = title;
+	    }
+
+	    public String getAuthor() {
+	        return author;
+	    }
+
+	    public void setAuthor(String author) {
+	        this.author = author;
+	    }
+
+	    public int getStateCode() {
+	        return stateCode;
+	    }
+
+	    public void setStateCode(int stateCode) {
+	        this.stateCode = stateCode;
+	    }
+
+	    // Rent method
+	    public void rent() {
+	        this.stateCode = 0; // Set stateCode to 0 (rented)
+	        System.out.println(this.title + "이(가) 대여 됐습니다.");
+	    }
+
+	    // Print method
+	    public void print() {
+	        System.out.print("책번호: " + this.bookNo + ", 제목: " + this.title + ", 작가: " + this.author);
+	        if (this.stateCode == 1) {
+	            System.out.println(", 상태: 재고있음");
+	        } else {
+	            System.out.println(", 상태: 대여중");
+	        }
+	    }
+	}
     
     
     
-    
-    
-}
+
